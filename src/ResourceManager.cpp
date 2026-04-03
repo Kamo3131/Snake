160209840990b08814b7c0f7d1d0bdb4f9499340
Kamo3131 <kacpermar2004@gmail.com>
2026-04-03 14:03:53 +0200

    fix(Resource manager)

--- src/ResourceManager.cpp
+++ src/ResourceManager.cpp
@@ -1,35 +1,40 @@
-#include <ResourceManager.hpp>
+#include "ResourceManager.hpp"
+#include <iostream>
 
 ResourceManager::ResourceManager() : textures() {}
 sf::Texture& ResourceManager::getTexture(std::string path) {
-    auto& pairFound = textures.find(path);
+    auto pairFound = textures.find(path);
     if(pairFound != textures.end()){
         return pairFound->second;
-    } else {
-        sf::Texture& texture = textures[path];
-        texture.loadFromFile(path);
-        return texture;
+    } 
+    
+    sf::Texture& texture = textures[path];
+    if (!texture.loadFromFile(path)) {
+        std::cout << "Texture " << path << " not loaded correctly!\n";
     }
+    return texture;
 }
 
 sf::Font& ResourceManager::getFont(std::string path) {
-    std::map<std::string,sf::Font>::iterator& pairFound = fonts.find(path);
+    std::map<std::string,sf::Font>::iterator pairFound = fonts.find(path);
     if(pairFound != fonts.end()) {
         return pairFound->second;
-    } else {
-        sf::Font& font = fonts[path];
-        font.openFromFile(path);
-        return font;
+    } 
+    sf::Font& font = fonts[path];
+    if (!font.openFromFile(path)){
+        std::cout << "Font " << path << " not loaded correctly!\n";
     }
+    return font;
 }
 
 sf::SoundBuffer& ResourceManager::getSound(std::string path) {
-    auto& pairFound = sounds.find(path);
+    auto pairFound = sounds.find(path);
     if(pairFound != sounds.end()) {
         return pairFound->second;
-    } else {
-        sf::SoundBuffer& sound = sounds[path];
-        sound.loadFromFile(path);
-        return sound;
+    } 
+    sf::SoundBuffer& sound = sounds[path];
+    if(!sound.loadFromFile(path)) {
+        std::cout << "Sound " << path << " not loaded correctly!\n";
     }
+    return sound; 
 }
