2712a2115c8b67dc082d8d2a750e12a1c84074f2
Kamo3131 <kacpermar2004@gmail.com>
2026-04-03 18:08:08 +0200

    feat: changes to Snake

--- include/Snake.hpp
+++ include/Snake.hpp
@@ -1,19 +1,26 @@
 #pragma once
 #include <iostream>
-#include <tuple>
+#include <vector>
+#include <deque>
+
+enum Direction {
+    UP, LEFT, RIGHT, DOWN
+};
 
 struct SnakeSegment {
-    SnakeSegment(short x, short y);
-    std::tuple<short, short> position;
+    SnakeSegment(short x, short y, Direction direction);
+    std::pair<short, short> m_position;
+    Direction m_direction = UP;
 };
 
 class Snake {
     private:
         int m_apples_eaten = 0;
-        int m_snake_segments = 2;
+        int m_segments_number = 2;
+        std::deque<SnakeSegment> m_snake_segments;
     public:
         Snake();
-        Snake(const int snake_segments);
+        Snake(const int segments_number);
         /**
          * @brief getter for apples eaten.
          * @returns number of apples eaten.
@@ -23,7 +30,7 @@ class Snake {
          * @brief getter for snake segments.
          * @returns number of snake segments.
          */
-        int getSnakeSegments() const;
+        int getSegmentsNumber() const;
         /**
          * @brief setter for apples eaten.
          * @param apples_eaten new value for m_apples_eaten.
@@ -33,7 +40,7 @@ class Snake {
          * @brief setter for snake segments.
          * @param snake_segments new value for m_snake_segments.
          */
-        void setSnakeSegments(const int snake_segments);
+        void setSegmentsNumber(const int segmentsNumber);
         /**
          * @brief makes snake grow.
          * @param new_segments dictate how many segments will be added to snake.
@@ -43,4 +50,6 @@ class Snake {
          * @brief increases apples eaten.
          */
         void eatApple();
+        std::pair<std::pair<short, short>, Direction> getSegment(int index) const;
+        void movement(std::pair<short, short> head_position, Direction head_direction);
 };
--- src/Snake.cpp
+++ src/Snake.cpp
@@ -1,25 +1,45 @@
 #include "Snake.hpp"
-SnakeSegment::SnakeSegment(short x, short y) : position{x, y}{}
+SnakeSegment::SnakeSegment(short x, short y, Direction direction) : m_position{x, y}, m_direction{direction}{}
 
-Snake::Snake() : m_apples_eaten{0}, m_snake_segments{2} {}
-Snake::Snake(const int snake_segments) : m_apples_eaten{0}, m_snake_segments{snake_segments} {
-
+Snake::Snake() : m_apples_eaten{0}, m_segments_number{2} {
+    m_snake_segments.emplace_back(50, 50, Direction::UP);
+    m_snake_segments.push_back(m_snake_segments.back());
+}
+Snake::Snake(const int segments_number) : m_apples_eaten{0}, m_segments_number{segments_number} {
+    m_snake_segments.emplace_back(50, 50, Direction::UP);
+    grow(segments_number-1);
 }
 int Snake::getApplesEaten() const {
     return m_apples_eaten;
 }
-int Snake::getSnakeSegments() const {
-    return m_snake_segments;
+int Snake::getSegmentsNumber() const {
+    return m_segments_number;
 }
 void Snake::setApplesEaten(const int apples_eaten) {
     m_apples_eaten = apples_eaten;
 }
-void Snake::setSnakeSegments(const int snake_segments) {
-    m_snake_segments = snake_segments;
+void Snake::setSegmentsNumber(const int segments_number) {
+    m_segments_number = segments_number;
 }
 void Snake::grow(const int new_segments) {
-    m_snake_segments += new_segments;
+    for(int i = 0; i < new_segments; ++i) {
+        m_snake_segments.push_back(m_snake_segments.back());
+    }
+    m_segments_number = m_snake_segments.size();
 }
 void Snake::eatApple(){
     ++m_apples_eaten;
+    grow(1);
+}
+
+std::pair<std::pair<short, short>, Direction> Snake::getSegment(int index) const {
+    SnakeSegment segment = m_snake_segments.at(index);
+    return std::make_pair(segment.m_position, segment.m_direction);
+}
+
+void Snake::movement(std::pair<short, short> head_position, Direction head_direction) {
+    m_snake_segments.emplace_front(head_position, head_direction);
+    if(m_snake_segments.size() > m_segments_number) {
+        m_snake_segments.pop_back();
+    }
 }
